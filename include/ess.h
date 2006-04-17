ce586179b70b3a81a2f9abe02ca8b429f9fb79b9
jet <jet>
2006-04-17 06:27:12 +0000

    added country code parameter to setup and moved a essserial_pci.c function here (and renamed it) for consistency

--- include/ess.h
+++ include/ess.h
@@ -1,7 +1,19 @@
 /* ess.h - header file for interfaces into essserial_hw code */
+/*
+ *  Copyright (C) 2006 Jeffrey Elliot Trull
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
 
 /* from essserial_hw.c */
-extern void esscom_hw_setup(u16, u16);
+extern void esscom_hw_setup(u16, u16, int);
 extern void esscom_hw_interrupt(void);
 extern void esscom_hw_timer_tick(struct linmodem_port *);
 extern void esscom_hw_shutdown(void);
+extern int  esscom_hw_check_modem(int vendor);
+
